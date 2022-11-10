c7c584e172c5c35e2272254ba75d79e5ef942f14
GustavoTT <guatavoteles1477@gmail.com>
2022-11-10 11:59:46 -0300

    Get Post Delete PRODUTOS

--- routes/produtos.js
+++ routes/produtos.js
@@ -8,21 +8,18 @@ const mysql   = require('../mysql').pool;
 router.get('/', (req, res, next) => { // Lista com todos os produtos
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error : error })} // Se tiver um erro retorna o próprio erro
-        conn.query('SELECT * FROM produtos;',
+        conn.query('SELECT * FROM tb_produtos;',
         (error, result, field) => {
             if (error) { return res.status(500).send({ error : error })} // Se tiver um erro retorna o próprio erro
             const response = {
                 quantidade: result.length,  // Conta quantos resultados geraram
                 produtos: result.map(prod => { // Modificando valor à retornar
                     return {
-                        id_produto: prod.id_produto,
-                        nome: prod.nome,
-                        preco: prod.preco,
-                        type_request: {
-                            tipo: 'GET',
-                            descricao: 'Retorna os detalhes de um produto específico',
-                            url: process.env.URL_API + '/produtos/' + prod.id_produto // URL individual do produto, ideal que seja var de ambiente
-                        }
+                        id_produto: prod.id,
+                        nome: prod.nome_prod,
+                        preco: prod.preco_prod,
+                        categoria: prod.categoria_prod,
+                        codigo: prod.cod_prod
                     }
                 })
             }
@@ -36,22 +33,19 @@ router.get('/', (req, res, next) => { // Lista com todos os produtos
 router.post('/', (req, res, next) => {
     mysql.getConnection((error, conn) => { // Pega uma conexão do pool de conexões
         if (error) { return res.status(500).send({ error : error })}
-        conn.query('INSERT INTO produtos (nome, preco) VALUES (?,?)',
-                    [req.body.nome, req.body.preco],
+        conn.query('INSERT INTO tb_produtos (nome_prod, preco_prod, categoria_prod, cod_prod, idUsuario) VALUES (?,?,?,?,?)',
+                    [req.body.nome_prod, req.body.preco_prod, req.body.categoria_prod, req.body.cod_prod, req.body.idUsuario],
                     (error, result, field) => { //callback
                         conn.release(); // Libera a conexão
                         if (error) { return res.status(500).send({ error : error })} // Se tiver um erro retorna o próprio erro
                         const response = {
                             mensagem: 'Produto inserido com sucesso',
                             produtoCriado: {
-                                id_produto: result.id_produto,
-                                nome: req.body.nome,
-                                preco: req.body.preco,
-                                type_request: {
-                                    tipo: 'GET',
-                                    descricao: 'Retorna todos os produto',
-                                    url: process.env.URL_API + '/produtos' // Ideal que seja var de ambiente
-                                }
+                                id_produto: result.id,
+                                nome: req.body.nome_prod,
+                                preco: req.body.preco_prod,
+                                categoria: req.body.categoria_prod,
+                                codigo: req.body.cod_prod
                             }
                         }
                         return res.status(201).send(response);
@@ -61,11 +55,11 @@ router.post('/', (req, res, next) => {
 
 
 // EXEMPLO GET, retorna os dados de um produto
-router.get('/:id_produto', (req, res, next) => { // Lista de um produto em específico
+router.get('/:id', (req, res, next) => { // Lista de um produto em específico
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error : error })} // Se tiver um erro retorna o próprio erro
-        conn.query('SELECT * FROM produtos WHERE id_produto = ?;',
-        [req.params.id_produto], // Pega do parâmetro lá em cima
+        conn.query('SELECT * FROM tb_produtos WHERE id = ?;',
+        [req.params.id], // Pega do parâmetro lá em cima
         (error, result, field) => {
             if (error) { return res.status(500).send({ error : error })} // Se tiver um erro retorna o próprio erro
             if (result.length == 0) {
@@ -75,14 +69,11 @@ router.get('/:id_produto', (req, res, next) => { // Lista de um produto em espec
             }
             const response = {
                 produto: {
-                    id_produto: result[0].id_produto,
-                    nome: result[0].nome,
-                    preco: result[0].preco,
-                    type_request: {
-                        tipo: 'GET',
-                        descricao: 'Retorna todos os produto',
-                        url: process.env.URL_API + '/produtos' // Ideal que seja var de ambiente
-                    }
+                    id_produto: result[0].id,
+                    nome: result[0].nome_prod,
+                    preco: result[0].preco_prod,
+                    categoria: result[0].categoria_prod,
+                    codigo: result[0].cod_prod
                 }
             }
             return res.status(200).send(response)
@@ -91,60 +82,16 @@ router.get('/:id_produto', (req, res, next) => { // Lista de um produto em espec
 });
 
 
-// EXEMPLO PATCH, altera um produto
-router.patch('/', (req, res, next) => {
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    };
-    mysql.getConnection((error, conn) => { // Pega uma conexão do pool de conexões
-        if (error) { return res.status(500).send({ error : error })}
-        conn.query(`UPDATE produtos
-                        SET nome  = ?,
-                            preco = ?
-                    WHERE id_produto = ?`,
-                    [req.body.nome, req.body.preco, req.body.id_produto],
-                    (error, result, field) => { //callback
-                        conn.release(); // Libera a conexão
-                        if (error) { return res.status(500).send({ error : error })} // Se tiver um erro retorna o próprio erro
-                        const response = {
-                            mensagem: 'Produto atualizado com sucesso',
-                            produtoAtualizado: {
-                                id_produto: req.body.id_produto,
-                                nome: req.body.nome,
-                                preco: req.body.preco,
-                                type_request: {
-                                    tipo: 'GET',
-                                    descricao: 'Retorna os detalhes de um produto específico',
-                                    url: process.env.URL_API + '/produtos/' + req.body.id_produto // URL individual do produto, ideal que seja var de ambiente
-                                }
-                            }
-                        }
-                        return res.status(202).send(response);
-                    })
-    });
-});
-
-
 // EXEMPLO DELETE, exclui um produto
 router.delete('/', (req, res, next) => {
     mysql.getConnection((error, conn) => { // Pega uma conexão do pool de conexões
         if (error) { return res.status(500).send({ error : error })}
-        conn.query(`DELETE FROM produtos WHERE id_produto = ?`, [req.body.id_produto],
+        conn.query(`DELETE FROM tb_produtos WHERE id = ?`, [req.body.id],
                     (error, result, field) => { //callback
                         conn.release(); // Libera a conexão
                         if (error) { return res.status(500).send({ error : error })} // Se tiver um erro retorna o próprio erro
                         const response = {
                             mensagem: 'Produto removido com sucesso',
-                            type_request: {
-                                tipo: 'POST',
-                                descricao: 'Insere um produto',
-                                url: process.env.URL_API + '/produtos',
-                                body_req: {
-                                    nome: 'String',
-                                    preco: 'Number'
-                                }
-                            }
                         }
                         return res.status(202).send(response);
                     })
